757b1d6e78022e9d8f9fcfbac2797ca0b33fba8b
Danny Hu <danny2168h@gmail.com>
2021-12-11 11:58:23 -0800

    code cleanup

--- app/src/main/java/com/project/todolist/screens/main/MainScreen.kt
+++ app/src/main/java/com/project/todolist/screens/main/MainScreen.kt
@@ -28,7 +28,6 @@ import androidx.compose.ui.semantics.semantics
 import androidx.compose.ui.semantics.testTag
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.ImeAction
-import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
--- app/src/main/java/com/project/todolist/screens/todolist/ListDetailedScreen.kt
+++ app/src/main/java/com/project/todolist/screens/todolist/ListDetailedScreen.kt
@@ -26,7 +26,6 @@ import androidx.compose.ui.semantics.testTag
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.ImeAction
-import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
