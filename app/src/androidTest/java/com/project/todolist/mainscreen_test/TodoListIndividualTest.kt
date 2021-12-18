7e629e76861cf8c046e3a028e7410ebd0693ae22
Danny Hu <danny2168h@gmail.com>
2021-12-17 18:53:04 -0800

    refactored UI tests

--- app/src/androidTest/java/com/project/todolist/mainscreen_test/TodoListIndividualTest.kt
+++ app/src/androidTest/java/com/project/todolist/mainscreen_test/TodoListIndividualTest.kt
@@ -1,13 +1,11 @@
 package com.project.todolist.mainscreen_test
 
-import androidx.compose.ui.test.SemanticsNodeInteraction
-import androidx.compose.ui.test.assertHasClickAction
-import androidx.compose.ui.test.assertTextContains
+import androidx.compose.ui.test.*
 import androidx.compose.ui.test.junit4.createAndroidComposeRule
-import androidx.compose.ui.test.onNodeWithTag
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import com.project.todolist.MainActivity
 import com.project.todolist.R
+import com.project.todolist.model.TodoItem
 import com.project.todolist.model.TodoList
 import com.project.todolist.screens.main.TodoListIndividual
 import org.junit.Before
@@ -21,10 +19,14 @@ class TodoListIndividualTest {
     @get:Rule
     val composeTestRule = createAndroidComposeRule<MainActivity>()
 
-    private val title = "Testing Todo"
-    private val todoList = TodoList(title = title)
+    private val title =
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum"
+    private val todoList =
+        TodoList(title = title, todoItems = listOf(TodoItem("1"), TodoItem("2"), TodoItem("3")))
     private lateinit var deleteIcon: SemanticsNodeInteraction
     private lateinit var individualList: SemanticsNodeInteraction
+    private lateinit var counter: SemanticsNodeInteraction
+    private lateinit var blackBar: SemanticsNodeInteraction
 
     @Before
     fun beforeEach() {
@@ -40,6 +42,14 @@ class TodoListIndividualTest {
             )
             individualList =
                 composeTestRule.onNodeWithTag(composeTestRule.activity.getString(R.string.individual_list))
+            counter = composeTestRule.onNodeWithTag(
+                composeTestRule.activity.getString(R.string.counter),
+                useUnmergedTree = true
+            )
+            blackBar = composeTestRule.onNodeWithTag(
+                composeTestRule.activity.getString(R.string.black_bar),
+                useUnmergedTree = true
+            )
         }
     }
 
@@ -56,6 +66,21 @@ class TodoListIndividualTest {
     @Test
     fun verifyCloseIconCanBeTapped() {
         deleteIcon.assertHasClickAction()
+    }
+
+    @Test
+    fun verifyCounterIsDisplayed() {
+        counter.assertTextEquals("${todoList.todoItems.size} items")
+    }
+
+    @Test
+    fun verifyLongTitleIsScrollable() {
+        composeTestRule.onNode(hasScrollAction(), useUnmergedTree = true).assertTextEquals(title)
+    }
+
+    @Test
+    fun verifyBlackBarVisible() {
+        blackBar.assertIsDisplayed()
     }
 
     private fun twoParam(num: Long, word: String) {
